196f06ead519173f87c0d7626a1751133d7e944c
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-10-01 14:13:18 -0700

    Started a function to create a tree based on given path.

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        if(args.length != 0){
+
+        }
+        else{
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please enter a directory to read from: ");
+            String path = in.nextLine();
+            File f = new File(path);
+            File[] files = f.listFiles();
+
+        }
+    }
+
+    public static TreeNode createTree(String path){
+        File f = new File(path);
+        File[] files;
+        if(!f.isDirectory())
+            return null;
+        else
+            files = 
+        TreeNode root = new TreeNode(f.getName());
         
+        return null;
     }
 }
